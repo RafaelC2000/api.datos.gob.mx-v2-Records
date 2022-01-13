7640d5c162ab743a3ecb7003a7ae616815f3efdc
Ernesto Cedillo <rafacrteto@outlook.com>
2022-01-13 14:55:22 -0600

    Deployt 2

--- api.js
+++ api.js
@@ -2,6 +2,7 @@ $tabla=document.querySelector('.tabla')
 $input=document.getElementById('input')
 $boton=document.querySelector('.btn')
 $botonB=document.querySelector('.btn-buscar')
+$botonO=document.querySelector('.btn-buscar2')
 $limpiar=document.querySelector('.limpiar')
 $limpiarB=document.querySelector('.limpiar2')
 $mensaje=document.querySelector('.mensaje')
@@ -66,4 +67,31 @@ $botonB.addEventListener('click', ()=>{
     console.log("buscar");
     console.log(id);
     buscarApi(id)
+})
+//------------------------Funciona ------------------------------
+const buscarOcid = async (ocid) => {
+    const resp = await fetch('https://api.datos.gob.mx/v2/Records')
+    const resp2 = await resp.json()
+
+    resp2.results.forEach(ele=>{
+        if(ele.ocid==ocid){
+                $mensaje.innerHTML=`
+                    <span>Registro:</span> ${ele.ocid} <br>
+                    <span>Encontrado con excito!!!</span>
+                `
+                $tabla.innerHTML += `
+                    <tr>
+                        <td><span>Resultado</span></td>
+                        <td><span>Id:</span> ${ele._id}</td> 
+                        <td><span>Ocid:</span> ${ocid}</td>
+                    </tr>
+                    ` 
+            }
+    })
+}
+$botonO.addEventListener('click', ()=>{
+    let ocid = document.getElementById('buscar2').value
+    console.log("buscar");
+    console.log(ocid);
+    buscarOcid(ocid)
 })
