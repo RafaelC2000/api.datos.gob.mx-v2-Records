51885790eda1c53794ce908439aa73616311a20f
Ernesto Cedillo <rafacrteto@outlook.com>
2022-01-13 14:46:41 -0600

    Deploy

---
+++ api.js
@@ -0,0 +1,69 @@
+$tabla=document.querySelector('.tabla')
+$input=document.getElementById('input')
+$boton=document.querySelector('.btn')
+$botonB=document.querySelector('.btn-buscar')
+$limpiar=document.querySelector('.limpiar')
+$limpiarB=document.querySelector('.limpiar2')
+$mensaje=document.querySelector('.mensaje')
+$elemento=document.querySelector('.elementoEncontrado')
+
+const api = async (i) => {
+    const resp = await fetch('https://api.datos.gob.mx/v2/Records')
+    const resp2 = await resp.json()
+
+    $tabla.innerHTML += `
+    <tr>
+        <td><span>${i}</span></td>
+        <td><span>Id:</span> ${resp2.results[i]._id}</td> 
+        <td><span>Ocid:</span> ${resp2.results[i].ocid}</td>
+    </tr>
+    ` 
+}
+$boton.addEventListener('click', ()=>{
+    let cantidad = ($input.value);
+    
+    for (let i = 0; i < cantidad; i++ ){
+        api(i)
+    }
+})
+$limpiar.addEventListener('click', ()=>{
+    $tabla.innerHTML = `
+    <tr>
+        
+    </tr>
+    ` 
+})
+$limpiarB.addEventListener('click', ()=>{
+    $tabla.innerHTML = `
+    <tr>
+        
+    </tr>
+    ` 
+    $mensaje.innerHTML = ``
+})
+const buscarApi = async (id) => {
+    const resp = await fetch('https://api.datos.gob.mx/v2/Records')
+    const resp2 = await resp.json()
+
+    resp2.results.forEach(ele=>{
+        if(ele._id==id){
+                $mensaje.innerHTML=`
+                    <span>Registro:</span> ${ele._id} <br>
+                    <span>Encontrado con excito!!!</span>
+                `
+                $tabla.innerHTML += `
+                    <tr>
+                        <td><span>Resultado</span></td>
+                        <td><span>Id:</span> ${id}</td> 
+                        <td><span>Ocid:</span> ${ele.ocid}</td>
+                    </tr>
+                    ` 
+            }
+    })
+}
+$botonB.addEventListener('click', ()=>{
+    let id = document.getElementById('buscar').value
+    console.log("buscar");
+    console.log(id);
+    buscarApi(id)
+})
